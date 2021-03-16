b793840e2d9feb9ff58b760178683a2e57733f4c
Dam Viet <vietdien2005@gmail.com>
2021-03-16 14:51:48 +0700

    optimize popup

--- src/lib/pages/popup-controller.js
+++ src/lib/pages/popup-controller.js
@@ -178,9 +178,6 @@ PopupController.prototype = {
     // Message text
     this.filteringMessageText = this._getTemplate("filtering-message-template");
 
-    // Footer
-    // this.footerDefault = this._getTemplate("footer-default-template");
-
     this._renderMain(containerMain, tabInfo);
     this._renderFilteringControls(containerMain);
   },
@@ -222,241 +219,6 @@ PopupController.prototype = {
     this._appendTemplate(container, template);
   },
 
-
-  _selectRequestTypesStatsData(stats, range) {
-    let result = {};
-
-    switch (range) {
-      case "day":
-        result = stats.lastMonth[stats.lastMonth.length - 1];
-        break;
-      case "week":
-        for (var i = 0; i < stats.lastWeek.length; i++) {
-          var d = stats.lastWeek[i];
-          for (var type in d) {
-            if (d[type]) {
-              result[type] = (result[type] ? result[type] : 0) + d[type];
-            }
-          }
-        }
-        break;
-      case "month":
-        result = stats.lastYear[stats.lastYear.length - 1];
-        break;
-      case "year":
-        for (var i = 0; i < stats.lastYear.length; i++) {
-          var d = stats.lastYear[i];
-          for (var type in d) {
-            if (d[type]) {
-              result[type] = (result[type] ? result[type] : 0) + d[type];
-            }
-          }
-        }
-        break;
-      default:
-        break;
-    }
-
-    return result;
-  },
-
-  _selectRequestsStatsData(stats, range, type) {
-    const result = [];
-    switch (range) {
-      case "day":
-        stats.today.forEach((d) => {
-          result.push(d[type]);
-        });
-        break;
-      case "week":
-        stats.lastWeek.forEach((d) => {
-          result.push(d[type]);
-        });
-        break;
-      case "month":
-        stats.lastMonth.forEach((d) => {
-          result.push(d[type]);
-        });
-        break;
-      case "year":
-        stats.lastYear.forEach((d) => {
-          result.push(d[type]);
-        });
-        break;
-      default:
-        break;
-    }
-    return result.map((val) => (val === undefined ? 0 : val));
-  },
-
-  DAYS_OF_WEEK: (function () {
-    return (
-      this.DAYS_OF_WEEK || [
-        i18n.getMessage("popup_statistics_week_days_mon"),
-        i18n.getMessage("popup_statistics_week_days_tue"),
-        i18n.getMessage("popup_statistics_week_days_wed"),
-        i18n.getMessage("popup_statistics_week_days_thu"),
-        i18n.getMessage("popup_statistics_week_days_fri"),
-        i18n.getMessage("popup_statistics_week_days_sat"),
-        i18n.getMessage("popup_statistics_week_days_sun"),
-      ]
-    );
-  })(),
-
-  _dayOfWeekAsString(dayIndex) {
-    return this.DAYS_OF_WEEK[dayIndex];
-  },
-
-  MONTHS_OF_YEAR: (function () {
-    return (
-      this.MONTHS_OF_YEAR || [
-        i18n.getMessage("popup_statistics_months_jan"),
-        i18n.getMessage("popup_statistics_months_feb"),
-        i18n.getMessage("popup_statistics_months_mar"),
-        i18n.getMessage("popup_statistics_months_apr"),
-        i18n.getMessage("popup_statistics_months_may"),
-        i18n.getMessage("popup_statistics_months_jun"),
-        i18n.getMessage("popup_statistics_months_jul"),
-        i18n.getMessage("popup_statistics_months_aug"),
-        i18n.getMessage("popup_statistics_months_sep"),
-        i18n.getMessage("popup_statistics_months_oct"),
-        i18n.getMessage("popup_statistics_months_nov"),
-        i18n.getMessage("popup_statistics_months_dec"),
-      ]
-    );
-  })(),
-
-  _monthsAsString(monthIndex) {
-    return this.MONTHS_OF_YEAR[monthIndex];
-  },
-
-  _getCategoriesLines(statsData, range) {
-    const now = new Date();
-    const day = now.getDay();
-    const month = now.getMonth();
-    const lastDayOfPrevMonth = new Date(
-      now.getFullYear(),
-      now.getMonth(),
-      0
-    ).getDate();
-
-    let categories = [];
-    const lines = [];
-    switch (range) {
-      case "day":
-        for (let i = 1; i < 25; i += 1) {
-          if (i % 3 === 0) {
-            const hour = (i + now.getHours()) % 24;
-            categories.push(hour.toString());
-            lines.push({
-              value: i - 1,
-            });
-          } else {
-            categories.push("");
-          }
-        }
-
-        break;
-      case "week":
-        for (let i = 0; i < 7; i += 1) {
-          categories.push(this._dayOfWeekAsString((day + i) % 7));
-          lines.push({
-            value: i,
-          });
-        }
-
-        break;
-      case "month":
-        for (let i = 0; i < 31; i += 1) {
-          if (i % 3 === 0) {
-            const c = ((i + now.getDate()) % lastDayOfPrevMonth) + 1;
-            categories.push(c.toString());
-            lines.push({
-              value: i,
-            });
-          } else {
-            categories.push("");
-          }
-        }
-
-        break;
-      case "year":
-        for (let i = 0; i < 13; i += 1) {
-          categories.push(this._monthsAsString((month + i) % 12));
-          categories = categories.slice(-statsData.length);
-          lines.push({
-            value: i,
-          });
-        }
-
-        break;
-    }
-
-    return {
-      categories,
-      lines,
-    };
-  },
-
-  _renderRequestsGraphs(stats, range, type) {},
-
-  _renderBlockedGroups(container, stats) {
-    const TOTAL_GROUP_ID = "total";
-
-    const timeRange = document.querySelector(".statistics-select-time").value;
-    const typeSelector = container.querySelector(".statistics-select-type");
-
-    const statsData = this._selectRequestTypesStatsData(stats, timeRange);
-
-    const getSelectTemplate = (group) =>
-      `<option value="${group.groupId}">${group.groupName}</option>`;
-
-    const blockedGroups = stats.blockedGroups.filter(
-      (group) => statsData[group.groupId]
-    );
-
-    if (blockedGroups.length === 0) {
-      const [totalBlockedGroup] = stats.blockedGroups.filter(
-        ({ groupId }) => groupId === TOTAL_GROUP_ID
-      );
-
-      typeSelector.insertAdjacentHTML(
-        "beforeend",
-        getSelectTemplate(totalBlockedGroup)
-      );
-      return;
-    }
-
-    blockedGroups.forEach((group) => {
-      typeSelector.insertAdjacentHTML("beforeend", getSelectTemplate(group));
-    });
-  },
-
-  _renderActions(container, tabInfo) {
-    const el = document.createElement("div");
-    el.classList.add("actions");
-
-    this._appendTemplate(el, this.actionOpenLogin);
-    this._appendTemplate(el, this.showUserInfo);
-    // this._appendTemplate(el, this.actionOpenAbuse);
-    // this._appendTemplate(el, this.actionOpenSiteReport);
-
-    if (!tabInfo.applicationAvailable) {
-      const disabledActionsSelectors = [
-        // "#siteReport",
-        "#openLogin",
-        // "#openAbuse",
-      ];
-      disabledActionsSelectors.forEach((selector) => {
-        const action = el.querySelector(selector);
-        action.classList.add("action_disabled");
-        action.setAttribute("aria-hidden", "true");
-      });
-    }
-
-    container.appendChild(el);
-  },
-
   _bindAction(parentElement, selector, eventName, handler) {
     let elements = [].slice.call(parentElement.querySelectorAll(selector));
     console.log(elements, selector, parentElement);
@@ -470,7 +232,7 @@ PopupController.prototype = {
     const parent = document.querySelector("#widget-popup");
 
     const self = this;
-    // this._bindAction(parent, "#siteReport", "click", (e) => {
+    // this._bindAction(parent, ".siteReport", "click", (e) => {
     //   e.preventDefault();
     //   if (!self.tabInfo.applicationAvailable) {
     //     return;
@@ -485,63 +247,13 @@ PopupController.prototype = {
       popupPage.closePopup();
     });
 
-    // this._bindAction(parent, ".openNotificationLink", "click", (e) => {
-    //   e.preventDefault();
-    //   const { url } = self.options.notification;
-    //   if (url) {
-    //     self.openLink(url);
-    //     popupPage.sendMessage({
-    //       type: "setNotificationViewed",
-    //       withDelay: false,
-    //     });
-    //     popupPage.closePopup();
-    //   }
-    // });
-
-    // this._bindAction(parent, ".closeNotification", "click", (e) => {
-    //   e.preventDefault();
-    //   const notification = parent.querySelector("#popup-notification");
-    //   if (notification) {
-    //     notification.style.display = "none";
-    //     popupPage.sendMessage({
-    //       type: "setNotificationViewed",
-    //       withDelay: false,
-    //     });
-    //   }
-    // });
-
-    // this._bindAction(parent, ".holiday-notify__btn", "click", (e) => {
-    //   e.preventDefault();
-    //   const { url } = self.options.notification;
-    //   if (url) {
-    //     self.openLink(url);
-    //     popupPage.sendMessage({
-    //       type: "setNotificationViewed",
-    //       withDelay: false,
-    //     });
-    //     popupPage.closePopup();
-    //   }
-    // });
-
-    // this._bindAction(parent, ".holiday-notify__close", "click", (e) => {
-    //   e.preventDefault();
-    //   const notification = parent.querySelector(".holiday-notify");
-    //   if (notification) {
-    //     notification.classList.add("holiday-notify--close");
-    //     popupPage.sendMessage({
-    //       type: "setNotificationViewed",
-    //       withDelay: false,
-    //     });
-    //   }
-    // });
-
     // this._bindAction(parent, ".openLink", "click", (e) => {
     //   e.preventDefault();
     //   self.openLink(e.currentTarget.href);
     //   popupPage.closePopup();
     // });
 
-    // this._bindAction(parent, "#openAbuse", "click", (e) => {
+    // this._bindAction(parent, ".openAbuse", "click", (e) => {
     //   e.preventDefault();
     //   if (!self.tabInfo.applicationAvailable) {
     //     return;
@@ -601,66 +313,38 @@ PopupController.prototype = {
       }
     });
 
-    // function changeProtectionState(disabled) {
-    //   const { tabInfo } = self;
-    //   if (tabInfo.applicationFilteringDisabled === disabled) {
-    //     return;
-    //   }
-    //   self.changeApplicationFilteringDisabled(disabled);
-    //   tabInfo.applicationFilteringDisabled = disabled;
-    //   tabInfo.totalBlockedTab = 0;
-    //   self._renderPopup(tabInfo);
-    //   self._bindActions();
-    //   self.resizePopupWindow();
-    // }
+    function changeProtectionState(disabled) {
+      const { tabInfo } = self;
+      if (tabInfo.applicationFilteringDisabled === disabled) {
+        return;
+      }
+      self.changeApplicationFilteringDisabled(disabled);
+      tabInfo.applicationFilteringDisabled = disabled;
+      tabInfo.totalBlockedTab = 0;
+      self._renderPopup(tabInfo);
+      self._bindActions();
+      self.resizePopupWindow();
+    }
 
     // Disable filtering
-    // const changeProtectionStateDisableButtons = [].slice.call(
-    //   document.querySelectorAll(".changeProtectionStateDisable")
-    // );
-    // changeProtectionStateDisableButtons.forEach((button) => {
-    //   button.addEventListener("click", (e) => {
-    //     e.preventDefault();
-    //     changeProtectionState(true);
-    //   });
-    // });
+    const changeProtectionStateDisableButtons = [].slice.call(
+      document.querySelectorAll(".changeProtectionStateDisable")
+    );
+    changeProtectionStateDisableButtons.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        e.preventDefault();
+        changeProtectionState(true);
+      });
+    });
 
     // Enable filtering
-    // const changeProtectionStateEnableButtons = [].slice.call(
-    //   document.querySelectorAll(".changeProtectionStateEnable")
-    // );
-    // changeProtectionStateEnableButtons.forEach((button) => {
-    //   button.addEventListener("click", (e) => {
-    //     e.preventDefault();
-    //     changeProtectionState(false);
-    //   });
-    // });
-
-    // Tabs
-    [].slice.call(parent.querySelectorAll(".tabbar .tab")).forEach((t) => {
-      t.addEventListener("click", (e) => {
+    const changeProtectionStateEnableButtons = [].slice.call(
+      document.querySelectorAll(".changeProtectionStateEnable")
+    );
+    changeProtectionStateEnableButtons.forEach((button) => {
+      button.addEventListener("click", (e) => {
         e.preventDefault();
-
-        [].slice
-          .call(parent.querySelectorAll(".tabbar .tab"))
-          .forEach((tab) => {
-            tab.classList.remove("active");
-          });
-        e.target.classList.add("active");
-
-        const attr = e.target.getAttribute("tab-switch");
-        [].slice
-          .call(parent.querySelectorAll(".tab-switch-tab"))
-          .forEach((tab) => {
-            tab.style.display = "none";
-          });
-        [].slice
-          .call(
-            parent.querySelectorAll(`.tab-switch-tab[tab-switch="${attr}"]`)
-          )
-          .forEach((tab) => {
-            tab.style.display = "flex";
-          });
+        changeProtectionState(false);
       });
     });
   },
@@ -677,15 +361,6 @@ PopupController.prototype = {
 };
 
 (function () {
-  /**
-   * TODO: check the following EDGE issue
-   * https://github.com/CyberPurify/PurifyBrowserExtension/issues/551
-   * MS Edge unexpectedly crashes on opening the popup.
-   * We do not quite understand the reason for this behavior,
-   * but we assume it happens due to code flow execution and changing the DOM.
-   * setTimeout allows us to resolve this "race condition".
-   */
-
   const controller = new PopupController();
   controller.afterRender = function () {
     // Add some delay for show popup size properly
